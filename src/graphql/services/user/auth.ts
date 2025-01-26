edc3e89e76d483c37a547f2948a34fb577c9703f
AdeolaFaraday <adexconly@gmail.com>
2025-01-26 08:20:08 +0100

    add social auth service

--- src/graphql/services/user/auth.ts
+++ src/graphql/services/user/auth.ts
@@ -1,6 +1,8 @@
 import { CreateUserInputs } from "../../../models/user/types/userAuth";
 import User from "../../../models/user/user";
+import SocialAuthService from "../../../services/auth/socialauth";
 import ClientResponse from "../../../services/response";
+import { generateSignUpUserData } from "../../../utlis/utlis";
 
 export default class AuthService {
   static async register(userInput: CreateUserInputs, context: any) {
@@ -47,5 +49,29 @@ export default class AuthService {
       password,
     });
     return new ClientResponse(200, true, 'Login successful', user);
+  }
+
+  static async socialAuth({ token }: { token: string }, context: any) {
+    try {
+      const socialAuthData = await SocialAuthService.verifyToken(token);
+      const userExist = await User.findOne({ email: socialAuthData?.email });
+
+      if (userExist) {
+        context.authenticate({
+          email: userExist.email,
+          password: JSON.stringify({
+            isValidated: true,
+          }),
+        });
+        return new ClientResponse(200, true, 'Login successful', userExist);
+      } else {
+        const userData = generateSignUpUserData(socialAuthData);
+        const newUser = await User.createUser(userData as CreateUserInputs);
+        return new ClientResponse(200, true, 'Login successful', newUser);
+      }
+    } catch (error) {
+      console.error('Error during social auth:', error);
+      return new ClientResponse(500, false, 'An error occurred during login', null);
+    }
   }
 }
