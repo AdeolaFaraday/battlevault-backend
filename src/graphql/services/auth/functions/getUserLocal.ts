24203926621a91ca70705dea19a3dbdb53c4774e
Adeola <adexconly@gmail.com>
2026-01-24 01:59:10 +0100

    add log for decode debug

--- src/graphql/services/auth/functions/getUserLocal.ts
+++ src/graphql/services/auth/functions/getUserLocal.ts
@@ -12,13 +12,17 @@ export const getUserLocal = (req: any, res: any) => {
         }
 
         try {
+            console.log({ gotIntoTryBlock: true });
             const decoded: any = verify(token, jwt.jwtSecret as string);
+            console.log({ decoded });
             const user = await User.findById(decoded.id);
+            console.log({ user });
             if (!user) {
                 return null;
             }
             return user;
         } catch (error) {
+            console.log({ errorBeforeReturnNull: error });
             return null;
         }
 
