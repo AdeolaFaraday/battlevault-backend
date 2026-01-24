b33560aeefdbff1bbfd852fc717271fe89820742
Adeola <adexconly@gmail.com>
2026-01-24 01:20:25 +0100

    fix check cookie bug

--- src/graphql/services/auth/functions/getUserLocal.ts
+++ src/graphql/services/auth/functions/getUserLocal.ts
@@ -6,12 +6,14 @@ export const getUserLocal = (req: any, res: any) => {
     return async () => {
         // Gets the currently logged in user from the request token;
         const token = req.cookies?.cookie || req.cookies?.user_token || req.cookies?.admin_token;
+        console.log({ USER_TOKEN: token });
         if (!token) {
             return null;
         }
 
         try {
             const decoded: any = verify(token, jwt.jwtSecret as string);
+            console.log({ decoded });
             const user = await User.findById(decoded.id);
             if (!user) {
                 return null;
